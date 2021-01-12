a80f49549ad220e0d4a45533e58fdc6fb83b5c3a
Leandro Pereira da Paixão <leandro.paixao@marttech.com.br>
2021-01-12 00:38:04 -0300

    desafio 3 - conceitos do ReactJS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Novo Repository ${Date.now()}`,
+      url: `http://github.com/teste/adicinar`,
+      techs: ["ReactJS, node"],
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    if (response.status !== 204) return;
+
+    const index = repositories.findIndex((repo) => repo.id === id);
+    if (index < 0) return;
+
+    repositories.splice(index, 1);
+    setRepositories([...repositories]);
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+      <div>
+        <ul data-testid="repository-list">
+          {repositories &&
+            repositories.map((repository) => {
+              const { title } = repository;
+              return (
+                <li key={repository.id}>
+                  { title }
+                  <button onClick={() => handleRemoveRepository(repository.id)}>
+                    Remover
+                  </button>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
